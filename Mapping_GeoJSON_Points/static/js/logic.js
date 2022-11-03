e039edbce379ebad2b5b5e6038e12c356819afcc
Tommy <baseballplayer987@hotmail.com>
2022-11-02 20:11:49 -0400

    mapping geojson

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,8 +1,5 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 10);
-
 // Add GeoJSON data.
 let sanFranAirport =
 {"type":"FeatureCollection","features":[{
@@ -28,14 +25,33 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: API_KEY
 });
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/tommy-chin/Mapping_Earthquakes/main/majorAirports.json";
 // Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  onEachFeature: function(feature, layer) {
-    console.log(layer);
-    layer.bindPopup("<h2>" + "Airport code: " + feature.properties.faa + "</h2>" + "Airport name: " + feature.properties.name);
-    
-  }
-}).addTo(map);
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
